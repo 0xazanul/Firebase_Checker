2abceb897fea42da395847346298542ade80a144
SURYESH <88741760+Suryesh@users.noreply.github.com>
2025-03-16 19:14:26 +0530

    Update firebase-checker.py

--- firebase-checker.py
+++ firebase-checker.py
@@ -71,8 +71,8 @@ def help():
     - Firebase Remote Config misconfigurations
 
     Usage:
-    -h, --help    python3 firebase-remote-extract_and_account_creation.py -h
-    To Run        python3 firebase-remote-extract_and_account_creation.py
+    -h, --help    python3 firebase-checker.py -h
+    To Run        python3 firebase-checker.py
     
     - Now Enter your apk or give directory path where your apk located like /home/{username}/path/gmail.apk
     - For more information, visit: https://github.com/Suryesh/Firebase_Checker
